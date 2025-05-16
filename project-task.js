d078784940c84a6b488fe6d473a8732b1d401db7
Christian Hoffman <choffman@RepTheatre.local>
2025-05-16 15:48:25 -0500

    Practice: Finally Block

--- project-task.js
+++ project-task.js
@@ -45,7 +45,15 @@ Step 4: Test Your Solution
 function processFile(fileName, fileData) {
   try {
     // TODO: Add input validation here
-    
+    if(!fileName) {
+      throw new Error("Missing filename");
+    }
+    if (typeof fileData !== "string") {
+      throw new Error("File data is not a string");
+    }
+    if (!fileData) {
+      throw new Error("File data is empty");
+    }
     // TODO: Implement simulated file processing here
     console.log(`Processing file: ${fileName}`);
     console.log(`File content: ${fileData}`);
@@ -54,9 +62,11 @@ function processFile(fileName, fileData) {
     
   } catch (err) {
     // TODO: Implement error handling
-    console.error(err);
+    console.error(err.message);
+  } finally {  
+    // TODO: Implement a finally block to close resources
+    console.log("Closing resources");
   }
-  // TODO: Implement a finally block to close resources
 }
 
 // ============================================
